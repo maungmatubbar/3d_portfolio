0419d8abb99f41bdad50eabc6e4635204ba3c222
maungmatubbar <maungmatubbbar@gmail.com>
2023-08-24 12:18:59 +0600

    added mouse click button

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -3,21 +3,12 @@ import { Html, useProgress } from "@react-three/drei";
 const CanvasLoader = () => {
   const { progress } = useProgress();
   return (
-    <Html
-      as="div"
-      center
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        flexDirection: "column",
-      }}
-    >
-      <span className="canvas-loader"></span>
+    <Html>
+      <span className="canvas-load"></span>
       <p
         style={{
-          fontSize: 14,
-          color: "#F1F1F1",
+          fontSize: 15,
+          color: "#f1f1f1",
           fontWeight: 800,
           marginTop: 40,
         }}
