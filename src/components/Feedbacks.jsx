eb6d38d099345555603c192dff13a7d10bb61c54
maungmatubbar <maungmatubbbar@gmail.com>
2023-08-30 11:52:45 +0600

    Added testimonial

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -1,9 +1,79 @@
-import React from 'react'
-
+import React from "react";
+import { styles } from "../style";
+import { SectionWrapper } from "../hoc";
+import { motion } from "framer-motion";
+import { fadeIn, textVariant } from "../utils/motion";
+import { testimonials } from "../constants";
+const FeedbackCard = ({
+  testimonial,
+  name,
+  index,
+  designation,
+  company,
+  image,
+  description,
+}) => {
+  return (
+    <motion.div
+      variants={fadeIn("", "spring", index & 0.5, 0.75)}
+      className="bg-black-200 p-10 rounded-3xl 
+    xs:w-[320px] w-full"
+    >
+      <p className="text-white font-black text-[48px]">"</p>
+      <div className="mt-1">
+        <p
+          className="
+        text-white tracking-wide 
+        text-[18px]"
+        >
+          {testimonial}
+        </p>
+        <div
+          className="mt-7 flex justify-between
+        items-center gap-1"
+        >
+          <div className="flex-1 flex flex-col">
+            <p
+              className="text-white font-medium
+            text-[16px]"
+            >
+              <span className="blue-text-gradient">@</span>
+              {name}
+            </p>
+            <p className="mt-1 text-secodary text-[12px]">
+              {description} of {company}{" "}
+            </p>
+          </div>
+          <img
+            src={image}
+            alt={`feeback-by-${name}`}
+            className="w-10 h-10 rounded-full 
+          object-cover"
+          />
+        </div>
+      </div>
+    </motion.div>
+  );
+};
 const Feedbacks = () => {
   return (
-    <div>Feedbacks</div>
-  )
-}
+    <div className="mt-12 bg-black-100 rounded-[20px]">
+      <div
+        className={`${styles.padding} bg-tertiary rounded-2xl
+       min-h-[300px]`}
+      >
+        <motion.div variants={textVariant()}>
+          <p className={styles.sectionSubText}>What Others say</p>
+          <h2 className={styles.sectionHeadText}>Testmonials</h2>
+        </motion.div>
+      </div>
+      <div className={`${styles.paddingX} -mt-20 pb-14 flex flex-wrap gap-7`}>
+        {testimonials.map((testimonial, index) => (
+          <FeedbackCard index={index} key={testimonial.name} {...testimonial} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default Feedbacks
+export default SectionWrapper(Feedbacks, "");
